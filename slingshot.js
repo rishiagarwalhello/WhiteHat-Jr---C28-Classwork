0b967b795e6bb9654299882e6ddb12dcad7280e1
Rishi Agarwal <ra9617516@gmail.com>
2021-01-18 22:58:06 +0530

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.body = Constraint.create(options);
+        this.pointB=pointB;
+        World.add(world, this.body);
+    }
+
+    fly()
+    {
+        this.body.bodyA = null;
+    }
+
+    display()
+    {
+        if(this.body.bodyA)
+        {
+            var pointA = this.body.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(3);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
